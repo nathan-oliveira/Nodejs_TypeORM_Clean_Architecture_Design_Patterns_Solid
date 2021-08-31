df0c37b3c747c6baba3621665eb40b6b0747dd51
Nathan Oliveira <nathan_oliveiramendonca@hotmail.com>
2021-08-31 14:22:17 -0300

    feat: ajuste make controller

--- src/main/factories/profile/make-profile-put-controller.ts
+++ src/main/factories/profile/make-profile-put-controller.ts
@@ -2,9 +2,11 @@ import { Controller } from '@/presentation/contracts'
 import { ProfilePutController } from '@/presentation/controllers'
 import { UserRepository } from '@/infra/repositories'
 import { UserService } from '@/data/services'
+import { BCrypt } from '@/presentation/helpers'
 
 export const makeProfilePutController = (): Controller => {
+  const bcrypt = new BCrypt()
   const repo = new UserRepository()
-  const service = new UserService(repo)
+  const service = new UserService(repo, bcrypt)
   return new ProfilePutController(service)
 }
