4dbfd3a4f176e954a30eac2766257f32c5194596
Nathan Oliveira <nathan_oliveiramendonca@hotmail.com>
2022-08-25 10:51:31 -0300

    feat: ajuste na env

--- src/main/config/env/env.ts
+++ src/main/config/env/env.ts
@@ -1,4 +1,4 @@
 export const env = {
-  port: process.env.PORT,
-  secret: process.env.APP_SECRET
+  port: process.env.PORT ?? 5000,
+  secret: process.env.APP_SECRET ?? 'e5e9fa1ba31ecd1ae84f75caaa474f3a663f05f4'
 }
