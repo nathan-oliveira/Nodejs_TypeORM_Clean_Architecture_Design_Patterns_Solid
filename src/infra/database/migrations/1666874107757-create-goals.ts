9a6b824349aa92d4da50aa195099dc88635034d8
Nathan Oliveira <nathan_oliveiramendonca@hotmail.com>
2022-12-01 15:57:52 -0300

    feat: criando migration gols

--- src/infra/database/migrations/1666874107757-create-goals.ts
+++ src/infra/database/migrations/1666874107757-create-goals.ts
@@ -1,11 +1,9 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm'
 
 export class createGoals1666874107757 implements MigrationInterface {
+  public async up (queryRunner: QueryRunner): Promise<void> {
+  }
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
-
+  public async down (queryRunner: QueryRunner): Promise<void> {
+  }
 }
